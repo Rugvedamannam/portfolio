f19adbc0d07e986d184cd396bb1e34a4ef712a4d
Rugveda Mannam <120186790+Rugvedamannam@users.noreply.github.com>
2025-02-23 12:24:36 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
             <div className={styles.body}>
                 <div className={styles.left}>
                     <p>
-                    I am a passionate and skilled software developer specializing in JavaScript, React, Node.js, and MongoDB. With a strong background in front-end development, I focus on building responsive, user-friendly web applications that provide seamless experiences.
+                    I am a passionate and skilled software developer specializing in JavaScript, React, and MongoDB. With a strong background in front-end development, I focus on building responsive, user-friendly web applications that provide seamless experiences.
                     </p>
                     <p>
                     My focus is on making web applications fast, efficient, and easy to scale. I’ve built real-time dashboards and event management platforms, working closely with teams to bring great ideas to life.
